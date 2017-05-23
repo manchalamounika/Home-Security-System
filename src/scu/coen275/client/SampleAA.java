885bd539b9d7f41ae5f1dc709c4b0d851986d7c3
manchalamounika <manchalamounika@gmail.com>
2017-05-23 07:12:58 -0700

    project structure commit

---
+++ src/scu/coen275/client/SampleAA.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package scu.coen275.client;
+
+/**
+ * @author santhosh
+ *
+ */
+public class SampleAA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First step of project!!!");
+	}
+
+}
